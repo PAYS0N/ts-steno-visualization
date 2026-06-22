efee973c3de369857b32eb9a6668e7c781b0f216
pays0n <63822744+PAYS0N@users.noreply.github.com>
2026-06-22 15:08:47 -0500

    Group keys

--- src/StenoKeyboard.tsx
+++ src/StenoKeyboard.tsx
@@ -2,34 +2,31 @@ import "./StenoKeyboard.css";
 
 function StenoKeyboard(): React.ReactElement {
 	return (
-		<>
+		<div className="board">
 			<div className="left-keys">
-				<div key="1">#</div>
-				<div key="2">S</div>
-				<div key="3">T</div>
-				<div key="4">K</div>
-				<div key="5">P</div>
-				<div key="6">W</div>
-				<div key="7">H</div>
-				<div key="8">R</div>
+				<div className="steno-key">#</div>
+				<div className="steno-key">S</div>
+				<div className="steno-key">T</div>
+				<div className="steno-key">K</div>
+				<div className="steno-key">P</div>
+				<div className="steno-key">W</div>
+				<div className="steno-key">H</div>
+				<div className="steno-key">R</div>
 			</div>
-			<div key="9">*</div>
-			<div key="10">*</div>
-			<div key="11">*</div>
-			<div key="12">*</div>
+			<div className="steno-key">*</div>
 			<div className="right-keys">
-				<div key="13">F</div>
-				<div key="14">R</div>
-				<div key="15">P</div>
-				<div key="16">B</div>
-				<div key="17">L</div>
-				<div key="18">G</div>
-				<div key="19">T</div>
-				<div key="20">S</div>
-				<div key="21">D</div>
-				<div key="22">Z</div>
+				<div className="steno-key">F</div>
+				<div className="steno-key">R</div>
+				<div className="steno-key">P</div>
+				<div className="steno-key">B</div>
+				<div className="steno-key">L</div>
+				<div className="steno-key">G</div>
+				<div className="steno-key">T</div>
+				<div className="steno-key">S</div>
+				<div className="steno-key">D</div>
+				<div className="steno-key">Z</div>
 			</div>
-		</>
+		</div>
 	)
 }
 
