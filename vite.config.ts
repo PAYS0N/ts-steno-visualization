db7da724d75d6e0c4b30c246265d80b4dc04bb22
pays0n <63822744+PAYS0N@users.noreply.github.com>
2026-06-24 13:53:19 -0500

    setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Served from https://pays0n.github.io/ts-steno-visualization/
+  base: '/ts-steno-visualization/',
   plugins: [react()],
 })
